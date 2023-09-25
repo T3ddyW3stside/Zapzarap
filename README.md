b65ffd38e30f520a1d3066569b2d6b20b4d2cc07
T3ddyW3stside <teddywestside6030@protonmail.com>
2023-09-25 11:31:34 +0200

    my first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+  ______                                     ______                       _        ______ _ _         _____      _ _           _             
+ |___  /                                    |  ____|                     (_)      |  ____(_) |       / ____|    | | |         | |            
+    / / __ _ _ __ ______ _ _ __ __ _ _ __   | |__ ___  _ __ ___ _ __  ___ _  ___  | |__   _| | ___  | |     ___ | | | ___  ___| |_ ___  _ __ 
+   / / / _` | '_ \_  / _` | '__/ _` | '_ \  |  __/ _ \| '__/ _ \ '_ \/ __| |/ __| |  __| | | |/ _ \ | |    / _ \| | |/ _ \/ __| __/ _ \| '__|
+  / /_| (_| | |_) / / (_| | | | (_| | |_) | | | | (_) | | |  __/ | | \__ \ | (__  | |    | | |  __/ | |___| (_) | | |  __/ (__| || (_) | |   
+ /_____\__,_| .__/___\__,_|_|  \__,_| .__/  |_|  \___/|_|  \___|_| |_|___/_|\___| |_|    |_|_|\___|  \_____\___/|_|_|\___|\___|\__\___/|_|   
+            | |                     | |                                                                                                      
+            |_|                     |_|                                                                                                      
+
+# Goal
+The script should enable cyber security analysts to easily collect various artifacts and send them to the desired destination.
+
+# Installation and run
+* Download PS script: git clone https://github.com/T3ddyW3stside/Zapzarap.git
+* Check if the src files contain the desired artifacts
+* Start PowerShell as administrator
+* Run: .\zapzerap.ps1 -[Light, Medium, High]
+
+# Future enhancements
+Implementation of the scp function to be able to send data via ssh
---
+++ zapzarap.ps1
@@ -0,0 +1,61 @@
+
+
+param (
+    [switch]$Light,
+    [switch]$Medium,
+    [switch]$High
+)
+
+Write-Host "****************************************************"
+Write-Host "    Zapzerap PowerShell file collector              "
+Write-Host "****************************************************"
+Write-Host ""
+
+# Src_Folder
+$lightSourceFolders = @(
+    (Join-Path -Path $env:USERPROFILE -ChildPath "Desktop\Source\src_light"),
+    (Join-Path -Path $env:USERPROFILE -ChildPath "Desktop\Source\src_light1"),
+    (Join-Path -Path $env:USERPROFILE -ChildPath "Desktop\Source\src_light2")
+)
+$mediumSourceFolders = @(
+    (Join-Path -Path $env:USERPROFILE -ChildPath "Desktop\Source\src_medium"),
+    (Join-Path -Path $env:USERPROFILE -ChildPath "Desktop\Source\src_medium1"),
+    (Join-Path -Path $env:USERPROFILE -ChildPath "Desktop\Source\src_medium2"),
+    (Join-Path -Path $env:USERPROFILE -ChildPath "Desktop\Source\src_medium3")
+)
+$highSourceFolders = @(
+    (Join-Path -Path $env:USERPROFILE -ChildPath "Desktop\Source\src_high"),
+    (Join-Path -Path $env:USERPROFILE -ChildPath "Desktop\Source\src_high1"),
+    (Join-Path -Path $env:USERPROFILE -ChildPath "Desktop\Source\src_high2"),
+    (Join-Path -Path $env:USERPROFILE -ChildPath "Desktop\Source\src_high3"),
+    (Join-Path -Path $env:USERPROFILE -ChildPath "Desktop\Source\src_high4")
+)
+
+# Zip_Files
+function ZipFolders($sourceFolders, $destinationFolder) {
+    foreach ($sourceFolder in $sourceFolders) {
+        $sourceFolderName = (Get-Item $sourceFolder).Name
+        $hostName = [System.Net.Dns]::GetHostName()
+        $zipFileName = "$sourceFolderName-$hostName.zip"
+
+        if (Test-Path -Path $sourceFolder -PathType Container) {
+            Compress-Archive -Path $sourceFolder -DestinationPath (Join-Path -Path $destinationFolder -ChildPath $zipFileName) -Force
+            Write-Host "Das Archiv wurde erfolgreich erstellt: $zipFileName"
+        } else {
+            Write-Host "Der angegebene Quellordner existiert nicht: $sourceFolder"
+        }
+    }
+}
+
+# Des_Folder
+if ($Light) {
+    ZipFolders $lightSourceFolders (Join-Path -Path $env:USERPROFILE -ChildPath "Desktop\dest")
+}
+
+if ($Medium) {
+    ZipFolders $mediumSourceFolders (Join-Path -Path $env:USERPROFILE -ChildPath "Desktop\dest")
+}
+
+if ($High) {
+    ZipFolders $highSourceFolders (Join-Path -Path $env:USERPROFILE -ChildPath "Desktop\dest")
+}
